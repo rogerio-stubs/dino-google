443610b532cf56b3ad61935b88d3414129a8be59
rogerio-stubs <rostubs17@gmail.com>
2020-06-08 20:35:40 -0300

    Utilizando o teclado

--- Game/Client/display.py
+++ Game/Client/display.py
@@ -1,21 +1,28 @@
 import pygame as pg
 
-def screen():        
+def screen():
     pg.init()
-    tela = pg.display.set_mode((1600,800)) # largura / altura
-    pg.display.set_caption("T-Rex Running")    
-    color = (255,255,255) # cor branca
+    tela = pg.display.set_mode((800, 800)) # largura / altura
+    pg.display.set_caption("T-Rex Running")
+    color = (255, 255, 255) # cor branca
     close = False
 
     while close != True:
         for event in pg.event.get():
             print('event', event)
+
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_UP:
+                    print('para cima')
+                if event.key == pg.K_DOWN:
+                    print('Para baixo')
+
             if event.type == pg.QUIT:
-                close = True               
+                close = True
 
-            tela.fill(color) # preenche a tela com a cor branca    
+            tela.fill(color) # preenche a tela com a cor branca
 
-            pg.display.update() 
+            pg.display.update()
 
     pg.quit()
 
