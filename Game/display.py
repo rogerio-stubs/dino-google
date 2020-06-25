ed08e96496a525ae9bf7634ec14c057c4d92992f
rogerio-stubs <rostubs17@gmail.com>
2020-06-25 20:27:22 -0300

    Matrizes de posicionamento

--- Game/display.py
+++ Game/display.py
@@ -5,17 +5,23 @@ import Server.dinosaur as dino
 import Server.cactus as cac
 
 DIC_PATH = os.path.abspath(os.path.dirname(__file__))
+CACTUS_DIMENSIONS = [[443, 0, 37, 72], [480, 0, 34, 72], [514, 0, 34, 72], [548, 0, 34, 72],
+                     [582, 0, 34, 72], [616, 0, 35, 72], [651, 0, 51, 102], [702, 0, 49, 102],
+                     [751, 0, 52, 102], [803, 0, 47, 102], [850, 0, 102, 102]]
 
+DINOSAUR_DIMENSIONS = [[1338, 0, 88, 96], [1426, 0, 88, 96], [1514, 0, 88, 96], [1602, 0, 88, 96],
+                       [1690, 0, 88, 96], [1778, 0, 88, 96], [1866, 0, 118, 96], [1984, 0, 120, 96]]
+
+PTERODACTYL_DIMENSIONS = [[259, 0, 93, 84], [352, 0, 92, 84]]
 
 def load_image(image_name):
     image = pg.image.load(DIC_PATH + image_name)
-    image_size = image.get_rect()
-    return image, image_size
+    image_scale = pg.transform.scale(image, [64, 64])
+    return image_scale
 
-def render(display, targer_dino, target_cactus):
+def render(display, target_dino, target_cactus):
     display.fill((255, 255, 255))
-    display.blit(targer_dino.image, [targer_dino.position_x, targer_dino.position_y])
-    # if target_cactus.show == True:
+    display.blit(target_dino.image, [target_dino.position_x, target_dino.position_y])
     display.blit(target_cactus.image, [target_cactus.position_x, target_cactus.position_y])
     pg.display.update()
 
@@ -25,12 +31,10 @@ def screen():
     display = pg.display.set_mode((800, 800)) # largura / altura
     pg.display.set_caption("T-Rex Running")
 
-    image_dino, dimensions_dino = load_image("/assets/dino.png")
-    image_dino = pg.transform.scale(image_dino, [64, 64])
+    image_dino = load_image("/assets/dino.png")
     t_rex = dino.Dinosaur(200, 350, image_dino)
 
-    image_cactus, dimensions_cactus = load_image("/assets/cactus.jpg")
-    image_cactus = pg.transform.scale(image_cactus, [32, 32])
+    image_cactus = load_image("/assets/cactus_general.png")
     cactus = cac.Cactus(750, 370, 5, image_cactus)
 
     close = False
